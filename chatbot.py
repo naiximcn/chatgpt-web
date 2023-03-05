d38530dc80caa683e8e9d5d764e15bf2ff2c3519
NaixiNana <87113988+naiximcn@users.noreply.github.com>
2023-03-05 22:48:26 +0800

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -2,7 +2,7 @@ import os
 import openai
 
 def chat(prompt):
-    openai.api_key = "想屁吃" #在这里替换你的API 如果没有去https://platform.openai.com/account/api-keys新建一个
+    openai.api_key = "想屁吃" #在这里替换你的API 如果没有去https://platform.openai.com/account/api-keys新建一个 注意：切勿泄露你的API 只建议在本地填入API
     response = openai.Completion.create(
         model="text-davinci-003",
         prompt=prompt,
